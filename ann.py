15c3660ff86f6696f6d36a493ec6b752ed6e9b75
Yohanes Gultom <yohanes.gultom@gmail.com>
2016-10-06 20:41:50 +0700

    add error viz

--- ann.py
+++ ann.py
@@ -1,5 +1,10 @@
+# Neural network
+# Author: yohanes.gultom@gmail.com
+# Reference http://iamtrask.github.io/2015/07/12/basic-python-network/
+
 import numpy as np
 import progressbar
+import matplotlib.pyplot as plt
 
 # dummy data
 X = np.array([[0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
@@ -10,7 +15,8 @@ syn0 = 2 * np.random.random((3, 4)) - 1
 syn1 = 2 * np.random.random((4, 1)) - 1
 
 # iterations / epochs
-epoch = 60000
+epoch = 800
+error_history = np.zeros(shape=(epoch, 2))
 bar = progressbar.ProgressBar(max_value=epoch)
 for j in xrange(epoch):
 
@@ -32,6 +38,9 @@ for j in xrange(epoch):
     syn1 += l1.T.dot(l2_delta)
     syn0 += X.T.dot(l1_delta)
 
+    # track MSE history
+    error_history[j, 0] = (l1_delta ** 2).mean()
+    error_history[j, 1] = (l2_delta ** 2).mean()
     bar.update(j)
 
 bar.finish()
@@ -39,7 +48,17 @@ print ''
 
 # print output layer rounded
 print 'Final error (MSE): '
-print (l2_delta ** 2).mean()
+print error_history[-1]
 print ''
 print 'Final output: '
-print np.around(l2, decimals=1).flatten()
+print np.rint(l2).flatten()
+
+# plot error
+f1 = plt.figure(1)
+layer0, = plt.plot(np.arange(epoch), error_history[:, 0], label='Layer 0')
+layer1, = plt.plot(np.arange(epoch), error_history[:, 1], label='Layer 1')
+plt.xlabel('Iteration')
+plt.ylabel('Mean Square Error')
+plt.legend(handles=[layer0, layer1])
+f1.show()
+plt.show()
